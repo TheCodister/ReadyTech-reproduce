358ceb6632cc8d9b5d3bb95d14b9b2ce06d36bf2
TheKodister <quang.nnguyen@katalon.com>
2025-12-12 11:49:40 +0700

    feat: add new testcase

---
+++ Object Repository/Page_Advanced Search - ePROVIDE - Online Su_e1d13d/svg_Search_icon.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>svg_Search_icon</name>
+   <tag></tag>
+   <elementGuidId>0167c984-1b9f-419b-95d5-fdc8fff3f69c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/following::*[name()='svg'][1]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.search__btn.btn.btn--primary.btn--icon.no-shrink > svg.icon</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=banner >> internal:role=button</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>140c55cb-a328-47a2-9abf-cee32556d728</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>icon</value>
+      <webElementGuid>b2062e7a-cd08-48c4-9185-7b62bf55a700</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-header[1]/header[@class=&quot;header page-center desktop&quot;]/div[@class=&quot;search&quot;]/div[@class=&quot;flex-layout&quot;]/button[@class=&quot;search__btn btn btn--primary btn--icon no-shrink&quot;]/svg[@class=&quot;icon&quot;]</value>
+      <webElementGuid>a3ef98ba-4d7d-468b-b5e0-40b46f284cad</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/following::*[name()='svg'][1]</value>
+      <webElementGuid>e010eedc-b8c5-4d9c-8999-5d8af8e5da7b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='English'])[1]/following::*[name()='svg'][3]</value>
+      <webElementGuid>b95347d2-7945-495d-97f2-b8980541363f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Advanced search'])[1]/preceding::*[name()='svg'][1]</value>
+      <webElementGuid>0c0784b8-1c8a-4179-9393-13263b347e9b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login'])[1]/preceding::*[name()='svg'][1]</value>
+      <webElementGuid>f4365890-fe25-4367-bd24-eef27c92a0c4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
