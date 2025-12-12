8a84aaf20c45237e8668b5c8f2bfe3603ec61fec
quangcuber002@gmail.com <katalon_g3_dummy_username>
2025-12-12 04:53:27 +0000

    Create "Openbrowser-and-navigate" test suite

---
+++ Test Suites/Openbrowser-and-navigate.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>Openbrowser-and-navigate</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>82df95c5-969b-4704-8c15-59c85b5dd46f</testSuiteGuid>
+    <testCaseLink>
+        <guid>3111de99-717a-42af-a241-d81a39552e11</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Openbrowser-and-navigate</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
